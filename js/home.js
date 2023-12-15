7614dacc4a52ea443132b1899b9596598c29fa69
tephyxp <lindacespedesg@gmail.com>
2023-12-15 01:18:58 +0100

    js dom home añadido

--- js/home.js
+++ js/home.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', async function () {
     const content = document.getElementById('content');
 
     // Crear sección para el logo
@@ -15,10 +15,39 @@ document.addEventListener('DOMContentLoaded', function() {
     const cardsSection = document.createElement('section');
     cardsSection.id = 'cards-section';
 
-    const cardsStack = document.createElement('div');
-    cardsStack.classList.add('cards-stack');
+    const cardContainer = document.createElement('div');
+    cardContainer.classList.add('card-container');
 
-    cardsSection.appendChild(cardsStack);
+    // Contenedor para la flecha izquierda
+    const leftArrowContainer = document.createElement('div');
+    leftArrowContainer.classList.add('arrow-container');
+
+    const leftButton = document.createElement('button');
+    leftButton.classList.add('left-button');
+    leftButton.innerHTML = '&#9664;'; // Botón izquierdo (flecha hacia la izquierda)
+
+    leftArrowContainer.appendChild(leftButton);
+
+    // Contenedor para la frase
+    const quoteContainer = document.createElement('div');
+    quoteContainer.classList.add('quote-container');
+
+    cardContainer.appendChild(leftArrowContainer);
+    cardContainer.appendChild(quoteContainer);
+
+    // Contenedor para la flecha derecha
+    const rightArrowContainer = document.createElement('div');
+    rightArrowContainer.classList.add('arrow-container');
+
+    const rightButton = document.createElement('button');
+    rightButton.classList.add('right-button');
+    rightButton.innerHTML = '&#9654;'; // Botón derecho (flecha hacia la derecha)
+
+    rightArrowContainer.appendChild(rightButton);
+
+    cardContainer.appendChild(rightArrowContainer);
+
+    cardsSection.appendChild(cardContainer);
 
     // Crear sección para los botones
     const buttonsSection = document.createElement('section');
@@ -39,4 +68,42 @@ document.addEventListener('DOMContentLoaded', function() {
     content.appendChild(logoSection);
     content.appendChild(cardsSection);
     content.appendChild(buttonsSection);
+
+    // URL de la API (reemplaza 'URL_DE_TU_API' por la dirección real de la API)
+    const apiUrl = 'https://uselessfacts.jsph.pl/api/v2/facts/random';
+
+    // Función para obtener la frase del día de la API
+    async function fetchQuoteOfTheDay() {
+        try {
+            const response = await fetch(apiUrl);
+            const data = await response.json();
+
+            return data.text; // Devuelve la frase del día obtenida de la API
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
+
+    // Función para mostrar la frase del día en el contenedor
+    async function displayQuoteOfTheDay() {
+        const quote = await fetchQuoteOfTheDay();
+
+        if (quote) {
+            quoteContainer.textContent = quote;
+
+            leftButton.addEventListener('click', async function () {
+                const newQuote = await fetchQuoteOfTheDay();
+                quoteContainer.textContent = newQuote;
+            });
+
+            rightButton.addEventListener('click', async function () {
+                const newQuote = await fetchQuoteOfTheDay();
+                quoteContainer.textContent = newQuote;
+            });
+        }
+    }
+
+    // Mostrar la frase del día cuando se cargue la página
+    await displayQuoteOfTheDay();
 });
+
